960987973c36c1a31b0a574798bc5e947ec61171
Andrej Mažari <andrejmazari1@gmail.com>
2022-01-04 17:45:54 +0100

    pridal som C

--- ahoj.py
+++ ahoj.py
@@ -1,6 +1,6 @@
 
 print("zadajte mena troch hracov ")
-d = input("hrac cislo 1 ")
-e = input("hrac cislo 2 ")
-f = input("hrac cislo 3 ")
-print("mena troch hracov su : ",d,",",e,",",f,"." )
+a = input("hrac cislo 1 ")
+b = input("hrac cislo 2 ")
+c = input("hrac cislo 3 ")
+print("mena troch hracov su : ",a,",",b,",",c,"." )
