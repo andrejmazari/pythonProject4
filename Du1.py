aed39d993f6699613e8061fb82baf514fc268c52
Andrej Mažari <andrejmazari1@gmail.com>
2022-01-05 10:06:38 +0100

    Du1

--- Du1.py
+++ Du1.py
@@ -2,3 +2,28 @@
 #uzivatel zada text
 #ak sa tam nachdza text, vypise v ktorom poli
 #ak sa nenachadza tak zada novy text
+
+x = ""
+z = x
+y = x
+
+cars = ["bmw", "audi", "skoda"]
+fruits = ["apple", "banana", "cherry"]
+fish = ["kapor", "stuka", "zubac"]
+
+while x not in fruits:
+    x = input("zadaj ovocie: ")
+else:
+    print(" našiel si ho")
+
+
+while z not in cars:
+    z = input("zadaj znacku auta: ")
+else:
+    print(" našiel si ho")
+
+while y not in fish:
+    y = input("traf nazov  ryby: ")
+else:
+    print(" trafil si ")
+
